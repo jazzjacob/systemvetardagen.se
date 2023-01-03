db0cba9f6d3223ed690d810d4b24aec78861951c
olehoef <ole.hoefener@t-online.de>
2023-01-03 17:13:12 +0100

    something...

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,69 @@
 <template>
-  <div class="wrapper">
-		<hero/>
-		<soon/>
-		<Button title="See Map" link="map"/>
+  	<div class="wrapper">
+		<div class="hero-bg">
+			<svg class="h-blob-1" width="325" height="224" viewBox="0 0 325 224" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M1.40052 81.0294C-2.96125 41.1577 16.1882 -21.0558 49.4841 -47.103C82.7799 -73.1503 130.368 -62.9161 182.529 -62.6998C234.837 -62.3682 292.041 -72.1665 313.666 -41.8895C335.616 -11.7247 322.165 58.2881 286.154 112.746C250.11 167.545 175.925 219.117 135.119 223.425C93.9889 227.845 86.384 185.117 60.3347 161.662C34.4644 137.98 6.08736 120.789 1.40052 81.0294Z" fill="#F2F3FF"/>
+			</svg>
+
+			<svg class="h-blob-2" width="325" height="289" viewBox="0 0 325 289" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M1.40052 146.182C-2.96125 106.311 16.1882 44.0971 49.4841 18.0498C82.7799 -7.99742 130.368 2.23669 182.529 2.45308C234.837 2.78459 292.041 -7.01366 313.666 23.2633C335.616 53.4281 322.165 123.441 286.154 177.898C250.11 232.698 175.925 284.269 135.119 288.577C93.9889 292.998 86.384 250.27 60.3347 226.815C34.4644 203.132 6.08736 185.942 1.40052 146.182Z" fill="#F2F3FF"/>
+			</svg>
+
+			<svg class="h-blob-3" width="146" height="124" viewBox="0 0 146 124" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M131.056 18.1338C144.639 34.2171 150.187 65.9946 142.535 86.7473C134.978 107.63 114.221 117.357 97.6731 121.508C81.0293 125.658 68.6899 124.361 51.8547 119.562C35.0196 114.634 13.7843 106.203 4.88847 85.0611C-4.00738 63.7896 -0.468165 29.8072 13.1147 13.7239C26.7933 -2.35944 50.4199 -0.413875 73.09 0.753461C95.76 1.79109 117.474 2.0505 131.056 18.1338Z" fill="#FFF3F9"/>
+			</svg>
+
+			<svg class="h-blob-4" width="194" height="212" viewBox="0 0 194 212" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M110.335 4.62613C85.159 -3.92985 44.1328 0.101435 25.2582 18.7175C6.38368 37.3336 9.72453 70.6486 6.35925 105.61C3.05768 140.685 -7.04341 177.609 10.8441 196.37C28.6381 215.332 74.2635 216.219 111.468 199.8C148.893 183.407 186.818 141.016 192.311 114.298C197.897 87.3792 171.114 76.2477 157.882 55.4884C144.492 34.8168 135.417 13.384 110.335 4.62613Z" fill="#FFF3F9"/>
+			</svg>
+
+			<svg class="h-blob-5" width="274" height="247" viewBox="0 0 274 247" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M212.289 32.5462C237.724 48.7384 264.314 63.7529 271.746 84.361C279.013 104.969 267.287 131.024 256.717 156.49C245.982 181.955 236.402 206.832 216.749 223.76C197.095 240.836 167.697 249.962 141.271 246.135C114.846 242.161 91.5586 225.38 67.1151 209.924C42.8368 194.32 17.0721 179.895 6.33681 159.139C-4.56365 138.384 -0.599845 111.152 12.1174 89.8075C24.9997 68.3161 46.4703 52.7128 68.9319 35.6375C91.3934 18.7093 114.681 0.456411 138.629 0.0148078C162.412 -0.573997 186.69 16.5013 212.289 32.5462Z" fill="#FFF3F9"/>
+			</svg>
+
+			<svg class="h-blob-6" width="166" height="176" viewBox="0 0 166 176" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M130.712 33.919C144.015 44.7824 159.822 50.2141 164.517 59.791C169.056 69.3679 162.483 83.0901 154.188 93.0958C146.05 103.244 136.033 109.82 126.017 124.971C116.158 140.123 106.298 163.851 89.7085 172.284C73.1191 180.717 49.9566 173.856 31.3328 161.135C12.8654 148.556 -0.750426 130.26 0.0320903 112.536C0.814607 94.6682 16.3084 77.2296 25.3856 59.934C34.4628 42.6383 37.2799 25.4856 48.0786 14.0505C58.8773 2.47246 77.6577 -3.24511 92.369 1.9007C106.924 7.18944 117.566 23.1986 130.712 33.919Z" fill="#F2F3FF"/>
+			</svg>
+
+			<svg class="h-blob-img" width="493" height="602" viewBox="0 0 616 753" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				<g filter="url(#filter0_dd_649_1183)">
+					<path d="M480.559 141.785C529.899 187.201 588.193 209.554 605.864 249.865C622.96 290.182 599.432 348.456 569.417 391.084C539.982 434.31 503.491 462.5 467.383 526.952C431.85 591.398 396.7 692.106 336.151 728.408C275.603 764.709 190.232 736.598 121.267 683.531C52.8832 631.063 2.06172 554.232 4.14393 479.259C6.21974 403.682 62.3424 329.348 94.9062 255.867C127.47 182.385 137.05 109.751 176.198 60.9827C215.339 11.6097 284.055 -13.2938 338.313 7.89295C392.002 29.6901 431.801 96.9669 480.559 141.785Z" fill="#FFF3F9" fill-opacity="0.2" shape-rendering="crispEdges"/>
+					<path d="M480.559 141.785C529.899 187.201 588.193 209.554 605.864 249.865C622.96 290.182 599.432 348.456 569.417 391.084C539.982 434.31 503.491 462.5 467.383 526.952C431.85 591.398 396.7 692.106 336.151 728.408C275.603 764.709 190.232 736.598 121.267 683.531C52.8832 631.063 2.06172 554.232 4.14393 479.259C6.21974 403.682 62.3424 329.348 94.9062 255.867C127.47 182.385 137.05 109.751 176.198 60.9827C215.339 11.6097 284.055 -13.2938 338.313 7.89295C392.002 29.6901 431.801 96.9669 480.559 141.785Z" fill="url(#pattern0)" fill-opacity="0.8" shape-rendering="crispEdges"/>
+				</g>
+				<defs>
+					<filter id="filter0_dd_649_1183" x="0.0820312" y="0.302246" width="615.547" height="752.472" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+						<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+						<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+						<feOffset dy="4"/>
+						<feGaussianBlur stdDeviation="2"/>
+						<feComposite in2="hardAlpha" operator="out"/>
+						<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+						<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_649_1183"/>
+						<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+						<feOffset dy="4"/>
+						<feGaussianBlur stdDeviation="2"/>
+						<feComposite in2="hardAlpha" operator="out"/>
+						<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+						<feBlend mode="normal" in2="effect1_dropShadow_649_1183" result="effect2_dropShadow_649_1183"/>
+						<feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_649_1183" result="shape"/>
+					</filter>
+				<pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+					<use xlink:href="#image0_649_1183" transform="translate(-0.59434) scale(0.00236104 0.00193424)"/>
+				</pattern>
+				<image id="image0_649_1183" width="927" height="517" xlink:href="@/assets/img/hero-img-provisional.png"></image>
+				</defs>
+			</svg>
+
+
+
+
+
+
+		</div>
+
+
+		
+
 	</div>
 </template>
 <style>
@@ -25,3 +86,56 @@ export default {
 		Button
 }
 </script>
+
+<style>
+	.hero-bg {
+		display: flex;
+		flex-direction: column;
+	
+	}
+
+	svg {
+		position: absolute;
+		display: flex;
+
+	}
+
+	svg.h-blob-1 {
+	
+		top: 0;
+		left: 350px;
+	}
+
+	svg.h-blob-2 {
+		top: 150px;
+		left: 30px;
+	}
+
+	svg.h-blob-3 {
+		top: 60px;
+		left: 1000px;
+	}
+
+	svg.h-blob-4 {
+		top: 260px;
+		left: 440px;
+	}
+
+	svg.h-blob-5 {
+		top: 550px;
+		left: 40px;
+	}
+
+	svg.h-blob-6 {
+		top: 550px;
+		left: 400px;
+	}
+
+	svg.h-blob-img {
+		left: 600px;
+		top: 100px;
+	}
+
+
+
+</style>
